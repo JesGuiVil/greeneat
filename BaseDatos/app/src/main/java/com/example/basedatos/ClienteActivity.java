7c5eb07e0a00dccd869eb92dfd05fc9e51b75574
jesguivil <ardijesus@gmail.com>
2023-11-14 16:06:52 +0100

    funcional menu lateral

--- BaseDatos/app/src/main/java/com/example/basedatos/ClienteActivity.java
+++ BaseDatos/app/src/main/java/com/example/basedatos/ClienteActivity.java
@@ -2,6 +2,7 @@ package com.example.basedatos;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,11 +10,13 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 import com.google.android.material.navigation.NavigationBarView;
+import com.google.android.material.navigation.NavigationView;
 
 public class ClienteActivity extends AppCompatActivity {
     InicioFragment inicioFragment = new InicioFragment();
     CarritoFragment carritoFragment = new CarritoFragment();
     CuentaFragment cuentaFragment = new CuentaFragment();
+    private NavigationView lateralNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +27,7 @@ public class ClienteActivity extends AppCompatActivity {
 
         navigation.setOnItemSelectedListener(mOnNavigationItemSelectedListener);
         loadFragment(inicioFragment);
+        lateralNavigationView = findViewById(R.id.lateral_navigation);
 
     }
 
@@ -39,6 +43,7 @@ public class ClienteActivity extends AppCompatActivity {
                     return true;
                 case "Cuenta":
                     loadFragment(cuentaFragment);
+                    showHideLateralNavigationView(true);
                     return true;
             }
             return false;
@@ -52,6 +57,15 @@ public class ClienteActivity extends AppCompatActivity {
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
         transaction.replace(R.id.frame_container, fragment);
         transaction.commit();
+    }
+    private void showHideLateralNavigationView(boolean show) {
+        if (show) {
+            lateralNavigationView.setVisibility(View.VISIBLE);
+            lateralNavigationView.setEnabled(true);
+        } else {
+            lateralNavigationView.setVisibility(View.GONE);
+            lateralNavigationView.setEnabled(false);
+        }
     }
 }
 
