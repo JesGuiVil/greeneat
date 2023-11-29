31f8018a89510bacd7d0b63e3ca86de3ad102184
adolforf98 <adolrfo@gmail.com>
2023-11-29 17:02:34 +0100

    introduccion loigca crud

--- BaseDatos/app/src/main/java/com/example/basedatos/ProductoFragment.java
+++ BaseDatos/app/src/main/java/com/example/basedatos/ProductoFragment.java
@@ -3,62 +3,84 @@ package com.example.basedatos;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link ProductoFragment#newInstance} factory method to
+ * Use the  factory method to
  * create an instance of this fragment.
  */
 public class ProductoFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public ProductoFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ProductoFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ProductoFragment newInstance(String param1, String param2) {
-        ProductoFragment fragment = new ProductoFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    // Existing code...
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_producto, container, false);
+        View view = inflater.inflate(R.layout.fragment_producto, container, false);
+
+        // Find buttons by their IDs
+        Button addButton = view.findViewById(R.id.aniadirproducto);
+        Button modifyButton = view.findViewById(R.id.modificarproducto);
+        Button deleteButton = view.findViewById(R.id.eliminarproducto);
+        Button showButton = view.findViewById(R.id.mostrarproducto);
+
+        // Set click listeners for each button
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Replace with the "Add Product" fragment
+                replaceFragment(new AnadirProductoFragment());
+            }
+        });
+
+        modifyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Replace with the "Modify Product" fragment
+                replaceFragment(new ModificarProductoFragment());
+            }
+        });
+
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Replace with the "Delete Product" fragment
+                replaceFragment(new BorrarProductoFragment());
+            }
+        });
+
+        showButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Replace with the "Show Product" fragment
+                replaceFragment(new MostrarProductoFragment());
+            }
+        });
+
+        return view;
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        // Get the FragmentManager
+        FragmentManager fragmentManager = requireActivity().getSupportFragmentManager();
+
+        // Start a new FragmentTransaction
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+
+        // Replace the current fragment with the new one
+        transaction.replace(R.id.fragment_container, fragment);
+
+        // Add the transaction to the back stack
+        transaction.addToBackStack(null);
+
+        // Commit the transaction
+        transaction.commit();
     }
 }
